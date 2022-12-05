0d2df33082097abac39ef1eb06f145c479c614a4
Gregory Collison <gregory.d.collison@gmail.com>
2022-12-04 21:52:00 -0500

    added js for column mouseover effect

--- connect4App.js
+++ connect4App.js
@@ -4,7 +4,7 @@ const topCells = document.querySelectorAll('.cell.row-top');
 const resetButton = document.querySelector('.reset');
 const statusSpan = document.querySelector('.status');
 
-// columns
+// Columns
 const column0 = [allCells[35], allCells[28], allCells[21], allCells[14], allCells[7], allCells[0], topCells[0]];
 const column1 = [allCells[36], allCells[29], allCells[22], allCells[15], allCells[8], allCells[1], topCells[1]];
 const column2 = [allCells[37], allCells[30], allCells[23], allCells[16], allCells[9], allCells[2], topCells[2]];
@@ -15,7 +15,7 @@ const column6 = [allCells[41], allCells[34], allCells[27], allCells[20], allCell
 const columns = [column0, column1, column2, column3, column4, column5, column6];
 
 
-// rows
+// Rows
 const topRow = [topCells[0], topCells[1], topCells[2], topCells[3], topCells[4], topCells[5], topCells[6]];
 const row0 = [allCells[0], allCells[1], allCells[2], allCells[3], allCells[4], allCells[5], allCells[6]];
 const row1 = [allCells[7], allCells[8], allCells[9], allCells[10], allCells[11], allCells[12], allCells[13]];
@@ -24,3 +24,48 @@ const row3 = [allCells[21], allCells[22], allCells[23], allCells[24], allCells[2
 const row4 = [allCells[28], allCells[29], allCells[30], allCells[31], allCells[32], allCells[33], allCells[34]];
 const row5 = [allCells[35], allCells[36], allCells[37], allCells[38], allCells[39], allCells[40], allCells[41]];
 const rows = [row0, row1, row2, row3, row4, row5, topRow];
+
+//Variables
+let gameIsLive = true;
+let yellowIsNext = true;
+
+
+//Functions
+const getClassListArray = (cell) => {
+    const classList = cell.classList;
+    return [...classList];
+};
+
+const getCellLocation = (cell) => {
+    const classList = getClassListArray(cell);
+    const rowClass = classList.find(className => className.includes('row'));
+    const columnClass = classList.find(className => className.includes('column'));
+
+    const rowIndex = rowClass[4];
+    const columnIndex = columnClass[4];
+
+    const rowNumber = parseInt(rowIndex, 10);
+    const columnNumber = parseInt(columnIndex, 10);
+
+    return [rowNumber, columnNumber];
+};
+
+//Event Handlers
+const handleCellMouseOver = (e) => {
+    const cell = e.target;
+    const [rowIndex, colIndex] = getCellLocation(cell)
+
+    const topCell = topCells[columnIndex];
+    if (yellowIsNext) {
+        topCell.classList.add('yellow');
+         } else {
+                topCell.classList.add('red');
+            }
+    };
+
+//Event Listeners
+for (const row of rows) {
+    for (const cell of row) {
+        cell.addEventListener('mouseover', handleCellMouseOver);
+    }
+};
